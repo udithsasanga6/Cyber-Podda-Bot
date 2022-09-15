d38aa05421c95a54dde928c4eeda67dbf626159f
udithsasanga6 <113581479+udithsasanga6@users.noreply.github.com>
2022-09-15 12:51:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ global.APIKeys = {
 
 
 global.OWNER_REACT = '🧑🏻‍💻'
-global.REACT_NUMBER = '94714188730'
+global.REACT_NUMBER = '12363004508'
 
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
@@ -34,23 +34,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94714188730'] //ur owner number
-global.ownername = "NIPUNA " //ur owner name
-global.ytname = "YT: Nipuna-apps" //ur yt chanel name
-global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['12363004508'] //ur owner number
+global.ownername = "𝐂𝐲𝐛𝐞𝐫 𝐏𝐨𝐝𝐝𝐚 " //ur owner name
+global.ytname = "YT: https://youtube.com/channel/UCWE9tUxQ_OPOl4Vd-HwrRsA" //ur yt chanel name
+global.socialm = "GitHub: udithsasanga6" //ur github or insta name
+global.location = "Srilanka, Colombo, Gampaha" //ur location
 
 //bot bomdy 
-global.owner = ['714188730']
-global.ownertag = '714188730' //ur tag number
-global.botname = '⚜𝓑𝓵𝓾𝓮 𝓛𝓲𝓸𝓷𝓮⚜' //ur bot name
-global.linkz = "https://github.com/Nipuna-apps/Blue-Lione-Bot.git" //your theme url which will be displayed on whatsapp
-global.websitex = "https://github.com/Nipuna-apps/Blue-Lione-Bot.git" //ur website to be displayed
+global.owner = ['12363004508']
+global.ownertag = '94778636881 //ur tag number
+global.botname = '⚜𝓒𝔂𝓫𝓮𝓻 𝓟𝓸𝓭𝓭𝓪⚜' //ur bot name
+global.linkz = "https://chat.whatsapp.com/ElVTsg2HjFH2cX8qpjQnFd" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCWE9tUxQ_OPOl4Vd-HwrRsA" //ur website to be displayed
 global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot.git' //script link
 global.reactmoji = "❤" //ur menu react emoji
 global.themeemoji = "🎸" //ur theme emoji
-global.packname = "⚜𝓑𝓵𝓾𝓮 𝓛𝓲𝓸𝓷𝓮⚜ Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "NIPUNA" //ur sticker watermark author
+global.packname = "⚜𝓒𝔂𝓫𝓮𝓻 𝓟𝓸𝓭𝓭𝓪⚜ Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "ᴄʏʙᴇʀ ᴘᴏᴅᴅᴀ" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -59,7 +59,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['714188730'] //ur premium numbers
+global.premium = ['12363004508'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
